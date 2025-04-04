25c493c829ad9f4eeca2be0d9eaa42dbc7cc8ac0
PoghosKeshishyan <poghos877@gmail.com>
2025-04-04 18:46:24 +0400

    global update

--- src/api_routes/footers.js
+++ src/api_routes/footers.js
@@ -5,7 +5,7 @@ const { prisma } = require('../../prisma/prisma-client');
 router.get('/', async (req, res) => {
     try {
         const footer = await prisma.footer.findMany();
-        res.status(200).json(footer);
+        res.status(200).json(footer[0]);
     } catch (error) {
         res.status(500).json({ message: 'Error while fetching data' });
     }
